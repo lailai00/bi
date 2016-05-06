ac42342dbd9c98faa9513e4a2554459376d53ebe
marsapolo <marsapolo@163.com>
2016-05-06 16:17:14 +0800

    no message

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -66,22 +66,51 @@ class Navbar extends React.Component {
               <div className="clear"></div>
             </div>
             <div className="titlesq">
-              <div>
-                <div className="t_sq_lg"></div>
-                <div><p><a href="http://e.weibo.com/gbasebi" target="blank">新浪微博</a></p></div>
+              <div className="titlesqtop10">
+                <div className="weibo"></div>
+                <div className="weibo1"><p><a href="http://e.weibo.com/gbasebi" target="blank">新浪微博</a></p></div>
                 <div className="clear"></div>
               </div>
-              <div className="weixindiv">
-                <div></div>
-                <div><p>微信公众号</p></div>
-                <div className="weixinimg"> </div>
+              <div className="weixindiv titlesqtop35">
+                <div className="weixin"></div>
+                <div className="weibo1"><p>微信公众号</p></div>
+                <div className="weixinimg"><div> </div>
                 <div className="clear"></div>
               </div>
             </div>
             <div className="clear"></div>
           </div>
         </div>
-      </nav>
+        <div id="main_index">
+          <div className="content">
+            <div id="bigtitle1"></div>
+            <div id="bigtitle2"><a href="#viewAny"></a></div>
+            <div id="bigtitle3"><a href="#dataAny"></a></div>
+            <div id="bigtitle4"><a href="#expandAny"></a></div>
+            <div id="bigtitle5"><a href="#integratedAny"></a></div>
+            <div id="bigtitle6"></div>
+          </div>
+        </div>
+    <div className="demostyle">
+      <div className="content">
+ 
+        <div className="demoDiv">
+          <div id="demo1" className="demoPic">
+            <div className="demoBg"></div>
+            <div className="demoShade"></div>
+            <div className="demoInfo">
+              <p>针对各数据类型采用不同的可视化方式，更准确的展现</p>
+              <p>数据隐含的信息。灵活的互动设计过程协助用户迅速获取分析结果。</p>
+              <div className="demoButton" >点击我就运行啦~</div>
+            </div>
+          </div>
+          <div className="demoTitle">天津市新房交易分析案例</div>
+        </div>
+
+     </div>
+     </div>
+     </div>
+    </nav>
       );
   }
 }
