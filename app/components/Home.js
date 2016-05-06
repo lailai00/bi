dd3f78c576ee11056ad2b266fc2ca2391d5d29f6
lailai00 <lailai00@hotmail.com>
2016-05-06 10:40:45 +0800

    初调

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,10 +3,126 @@ import React from 'react';
 class Home extends React.Component {
   render() {
     return (
-      <div className='alert alert-info'>
-        Hello from Home Component
-      </div>
+
+	    <div id="mainPage" className="mainDiv">
+	      <div className='alert alert-info'>
+			<div id="viewAny" className="con_body whiteBody">
+				<div className="content">
+				
+					<div id="view1"  className="con_sub_title">
+						 <h1>全过程可视化 </h1>
+						 <p>采用“可视化”的设计思想和架构，创造性的使复杂的商业智能变得简便易用。以全面的视角揭示数据间的关系、趋势和重要KPI指标，并提供阅览图表内容时的互动分析，比传统报表能更深入的探求数据价值，协助用户高效、准确的决策及预测。贴合业务的数据模型与简洁直观的操作方式，使用户可以迅速掌握，适用于组织中的各层级成员。</p>
+					</div>
+					
+					<div id="view2" >
+						 <h2>可视化数据建模 </h2>
+						 <p>采用可视化的方式，在创建复杂的数据模型时，<br/>只需点选拖拽即可完成，无需编程，简单易用。 </p>
+					</div>
+					
+					<div id="view2img" >
+						  
+					</div>
+					
+					<div id="view3" >
+						 <h2>可视化分析应用设计</h2>
+						 <p>“可视化分析应用设计器”不仅可以分析数据，还能将<br/>数据和分析结果以多种形式可视化，并辅以交互动作和<br/>体验设计的强大功能，创造卓越的“可视化分析应用”。
+						 </p>
+					</div>
+					
+					<div id="view3img" >
+						  
+					</div>
+					
+					<div id="view4" >
+						 <h2>支持多平台的分析应用 </h2>
+						 <p>除了利用电脑，还能使用移动设备。即使脱<br/>离网络，仍然可以阅览已经下载的分析成果，<br/>继续您的分析工作。</p>
+					</div>
+					<div id="view4img" >
+						  
+					</div>
+				</div>
+			</div>
+	      </div>
+	      <div id="dataAny" className="con_body grallyBody">
+				<div className="content">
+				
+					<div id="data1" className="con_sub_title" >
+						 <h1>整合多数据源形成全数据分析</h1>
+						 <p>可将分布于不同物理位置、不同存储介质、不同数据（仓）库和本地数据文件的数据纳入统一管理，根据数据所蕴含的业务关系重新进行梳理和划分，构建符合业务逻辑的数据主题，搭建贴合实际数据分析需要的数据立方体，并依据具体数据结构创建真实或虚拟的维度、度量，建立符合业务逻辑的语义模型。经过语义映射，组织内各成员均可清晰理解已有数据的含义，无需了解数据库结构和IT知识，即可快速查询与分析数据。</p>
+					</div>
+					
+					<div id="data2" >
+						 <h2>可整合异构数据源 </h2>
+						 <p>支持GBase8a、MySQL、Oracle、hadoop<br/>等主流数据库，以及EXCEl、CSV文件，对多<br/>数据源进行整合。</p>
+					</div>
+					
+					<div id="data2img" >
+						  
+					</div>
+					
+					<div id="data3" >
+						 <h2>数据可抽象为语义模型</h2>
+						 <p>“单表”、“星型”、“雪花”型的数据结构均可以<br/>构建数据模型，并能够使用函数、表达式和预测分析<br/>模型增加“虚度量”和“虚维度”，进行语义映射。 </p>
+					</div>
+					
+					<div id="data3img" >
+						  
+					</div>
+					
+					<div id="data4" >
+						 <h2>高效的数据分析工具</h2>
+						 <p>语义映射构建的面向业务的数据模型，让所有使用者都可以简单高效的使用“吉查”、“吉报”和“吉图”查询和分析数据，而无需了解数据结构。</p>
+					</div>
+					<div id="data4img" >
+						  
+					</div>
+				</div>
+			</div>
+			<div id="expandAny" className="con_body whiteBody">
+				<div className="content">
+				
+					<div id="expand1"  className="con_sub_title">
+						 <h1>通过多种形式进行扩展</h1>
+						 <p>从可视化分析到数据库处理流程的多个节点上，系统提供了极其灵活的扩展接口，不论是增加可视化组件、复杂函数公式、预测模型PMML，还是支持新的数据库类型等要求，GBaseBI均可以灵活的以不同形式进行扩展，满足项目实施和持续使用过程中不断增加的新需求。</p>
+					</div>
+					
+					 
+					<div id="expandimg" >
+					</div> 
+				</div>
+			</div>
+			
+			<div id="integratedAny" className="con_body grallyBody">
+				<div className="content">
+				
+					<div id="integrated1"  className="con_sub_title">
+						<h1>灵活支持应用集成</h1>
+							 <p >采用当前主流的基于SOA的B/S架构，提供多种集成方案，各功能模块均可独立嵌入用户系统，还可共享用户的GIS、CMS、用户管理等系统或服务，维持已有业务逻辑，简化IT架构，以降低系统维护的复杂度及用户学习成本。 </p>
+							 <p >无需改变用户系统的主要架构和操作方式，即可抛弃数据来源单一、功能不足、展现效果差、使用困难等陈旧的数据查询与分析模式，升级为功能完善、展示绚丽、极致用户体验、以可视化分析为核心的全新用户系统。 
+						</p>
+					</div>
+					
+					 
+					<div id="integrated1img" >
+					</div> 
+					<div id="integrated2img" >
+					</div> 
+				</div>
+			</div>
+			
+	    </div>
     );
+
+
+
+
+
+
+
+
+
+
+
   }
 }
 
